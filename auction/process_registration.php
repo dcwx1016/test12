8c63e930cc791abbff865306b6f51dcd71f71c6b
Yang Zou <dcwx1016@gmail.com>
2020-11-05 21:41:52 +0000

    Insert user email and password into database.

--- auction/process_registration.php
+++ auction/process_registration.php
@@ -1,7 +1,12 @@
-<?php
+<?php include_once("header.php");
 
 // TODO: Extract $_POST variables, check they're OK, and attempt to create
 // an account. Notify user of success/failure and redirect/give navigation 
 // options.
 
-?>
+//$mail = $_POST['email'];
+//$pass = $_POST['password'];
+$connection = mysqli_connect('localhost','yangzou','123','auction_13');
+$query = "INSERT INTO Users (Email, Password) VALUES ('yang@123','1111')";
+$result = mysqli_query($connection, $query);
+mysqli_close($connection);
